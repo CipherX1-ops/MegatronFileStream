e3802a9f984c1edfada437bd384469df7fce32b9
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-06-06 02:40:12 +0430

    Update vars.py

--- Megatron/vars.py
+++ Megatron/vars.py
@@ -5,16 +5,16 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(getenv('API_ID', 1222520))
-    API_HASH = str(getenv('API_HASH', '7e7e46cdd61842035fb191013f0a2387'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '1489398806:AAFV7_v2UxCGXavz0kjFV6L2CjiUHA7KNmI'))
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'Megatron'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', -1001452480828))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(getenv('OWNER_ID', '1601105531'))
+    OWNER_ID = int(getenv('OWNER_ID'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
@@ -22,8 +22,8 @@ class Var(object):
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://cipherx:cipherx@cipherx.suvut.mongodb.net/cipherxretryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'FutureTechnologyOfficial'))
+    DATABASE_URL = str(getenv('DATABASE_URL'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-100")).split()))
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
