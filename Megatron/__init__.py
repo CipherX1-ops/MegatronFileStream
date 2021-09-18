22dddb5a9e0d042ae1fa0e8a1b5a82bf33a67f1d
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-09-19 02:36:49 +0430

    Update __init__.py

--- Megatron/__init__.py
+++ Megatron/__init__.py
@@ -1,4 +1,11 @@
 import time
+from .bot import StreamBot
+
+print('\n')
+print('------------------- Initalizing Telegram Bot -------------------')
+
+StreamBot.start()
+bot_info = StreamBot.get_me()
 
 __version__ = 1.02
 StartTime = time.time()
