f46b42cfd4555df93b7bf38b75e203b933c80dde
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-06 14:30:45 +0330

    Update nim.py

--- Megatron/bot/plugins/nim.py
+++ Megatron/bot/plugins/nim.py
@@ -53,15 +53,20 @@ async def nimdownloader(c: Client, m: Message):
         if "http" in url.text.lower():
             break
     try:
-        url = "https://www.digitalbam.ir/DirectLinkDownloader/Download"
-        data = {"downloadUri":txt}
-        request = post(url,data).json()["fileUrl"]
+        #url = "https://www.digitalbam.ir/DirectLinkDownloader/Download"
+        #data = {"downloadUri":txt}
+        #request = post(url,data).json()["fileUrl"]
+        url = f"https://rimon.ir/api/?url={txt}"
+        requests = post(url).json()
+        link1 = requests["dl1"] 
+        link2 = requests["dl2"] 
+        link3 = requests["dl3"] 
         log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-        await log_msg.reply_text(text=f"Requested by [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID:** `{m.from_user.id}`\n**Requested Link:** {txt}\n**Download Link:** {request}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
+        await log_msg.reply_text(text=f"Requested by [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID:** `{m.from_user.id}`\n**Requested Link:** {txt}\n**Download Links:**\n✨ {link1}\n✨ {link2}\n✨ {link3}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
         msg = "لینک نیم بهای شما ایجاد شد 😄\n\n⚜️ لینک درخواستی شما : [لینک]({})\n\n⚜️ لینک نیم بهای شما : [لینک]({})\n\n✨ @FiletoLinkTelegramBot ✨"
         await m.reply_text(
-            text=msg.format(txt, request),
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✵ Download Now ✵", url=request)]]),
+            text=msg.format(txt, link1, link2, link3),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✵ Server 1 ✵", url=link1)], [InlineKeyboardButton("✵ Server 2 ✵", url=link2)], [InlineKeyboardButton("✵ Server 3 ✵", url=link3)]]),
             quote=True
         )
     except Exception as e:
