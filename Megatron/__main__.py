b772b7c932a0a7eea711a73a4ce53da7627df096
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-12-24 14:55:37 +0330

    Update __main__.py

--- Megatron/__main__.py
+++ Megatron/__main__.py
@@ -11,14 +11,13 @@ from .vars import Var
 from aiohttp import web
 from .server import web_server
 from .utils.keepalive import ping_server
-from apscheduler.schedulers.background import BackgroundScheduler
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 
 ppath = "Megatron/bot/plugins/*.py"
@@ -45,9 +44,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('------------------ Starting Keep Alive Service ------------------')
         print('\n')
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(ping_server, "interval", seconds=1200)
-        scheduler.start()
+        await asyncio.create_task(ping_server())
     print('-------------------- Initalizing Web Server --------------------')
     app = web.AppRunner(await web_server())
     await app.setup()
