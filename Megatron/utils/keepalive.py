4c56d97fe3ba3e11f7a52949d90d8a50ff397040
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-12-29 13:20:50 +0330

    Update keepalive.py

--- Megatron/utils/keepalive.py
+++ Megatron/utils/keepalive.py
@@ -7,25 +7,22 @@ from os import environ
 from Megatron.vars import Var 
 
 async def ping_server():
-    sleep_time = Var.PING_INTERVAL
-    while True:
-        await asyncio.sleep(sleep_time) 
-        try:
-            session = aiohttp.ClientSession(raise_for_status=True)
-            x = await session.get(Var.URL, raise_for_status=False)
-            async with x:
-                if x.status == 200:
-                    logging.info(f"Bot is working fine. Pinged server with response code : {x.status}") 
+    try:
+        session = aiohttp.ClientSession(raise_for_status=True)
+        x = await session.get(Var.URL, raise_for_status=False)
+        async with x:
+            if x.status == 200:
+                logging.info(f"Bot is working fine. Pinged server with response code : {x.status}") 
+            else:
+                if 'DYNO' in environ: 
+                    try:
+                        heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+                        app = heroku.app(Var.APP_NAME)
+                        app.restart()
+                        logging.info("Successfully restarted the app for pinging...") 
+                    except Exception as e:
+                        logging.warning(f"Failed to restart the app because of this error : {str(e)}") 
                 else:
-                    if 'DYNO' in environ: 
-                        try:
-                            heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-                            app = heroku.app(Var.APP_NAME)
-                            app.restart()
-                            logging.info("Successfully restarted the app for pinging...") 
-                        except Exception as e:
-                            logging.warning(f"Failed to restart the app because of this error : {str(e)}") 
-                    else:
-                        logging.warning("Failed to ping the server...!")
-        except Exception:
-            traceback.print_exc()
+                    logging.warning("Failed to ping the server...!")
+    except Exception:
+        traceback.print_exc()
