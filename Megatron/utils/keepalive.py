1f4bb6bd2935a586e3c03d9ec618c1d036c59824
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-08 14:12:06 +0330

    Update keepalive.py

--- Megatron/utils/keepalive.py
+++ Megatron/utils/keepalive.py
@@ -1,28 +1,22 @@
-import asyncio 
+import asyncio
 import logging
 import aiohttp
 import traceback
-from os import environ
 
-from Megatron.vars import Var 
+from Megatron import Var
+
 
 async def ping_server():
-    try:
-        session = aiohttp.ClientSession(raise_for_status=True)
-        x = await session.get(Var.URL, raise_for_status=False)
-        async with x:
-            if x.status == 200:
-                logging.info(f"Bot is working fine. Pinged server with response code : {x.status}") 
-            else:
-                if 'DYNO' in environ: 
-                    try:
-                        heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-                        app = heroku.app(Var.APP_NAME)
-                        app.restart()
-                        logging.info("Successfully restarted the app for pinging...") 
-                    except Exception as e:
-                        logging.warning(f"Failed to restart the app because of this error : {str(e)}") 
-                else:
-                    logging.warning("Failed to ping the server...!")
-    except Exception:
-        traceback.print_exc()
+    sleep_time = Var.PING_INTERVAL
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(Var.URL) as resp:
+                    logging.info("Pinged server with response: {}".format(resp.status))
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
