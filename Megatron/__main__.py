b1a9f3e6ba64f27af62f0f857001edb128705852
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-08 14:02:11 +0330

    Update __main__.py

--- Megatron/__main__.py
+++ Megatron/__main__.py
@@ -1,72 +1,56 @@
-import os
-import sys
-import glob
 import asyncio
 import logging
-import importlib
-from pathlib import Path
-from apscheduler.schedulers.background import BackgroundScheduler
-
-from pyrogram import idle 
-
-from Megatron import bot_info
 from .vars import Var
 from aiohttp import web
-from .server import web_server
-from .utils.keepalive import ping_server
+from pyrogram import idle
+from Megatron import utils
+from Megatron import bot_info
+from Megatron.server import web_server
+from Megatron.bot.clients import initialize_clients
+
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 loop = asyncio.get_event_loop()
 
-ppath = "Megatron/bot/plugins/*.py"
-files = glob.glob(ppath)
 
 async def start_services():
-    print('----------------------------- DONE -----------------------------')
-    print('\n')
-    print('--------------------------- Importing ---------------------------')
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem.replace(".py", "")
-            plugins_dir = Path(f"Megatron/bot/plugins/{plugin_name}.py")
-            import_path = ".plugins.{}".format(plugin_name)
-            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
-            load = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(load)
-            sys.modules["Megatron.bot.plugins." + plugin_name] = load
-            print("Imported => " + plugin_name)
-    print('------------------ Starting Keep Alive Service ------------------')
-    print('\n')
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(ping_server, "interval", seconds=int(Var.PING_INTERVAL))
-    scheduler.start()
-    print('-------------------- Initalizing Web Server --------------------')
+    print("----------------------------- DONE -----------------------------")
+    print()
+    print(
+        "----------------------------- Initializing Clients -----------------------------"
+    )
+    await initialize_clients()
+    print("----------------------------- DONE -----------------------------")
+    if Var.ON_HEROKU:
+        print("------------------ Starting Keep Alive Service ------------------")
+        print()
+        asyncio.create_task(utils.ping_server())
+    print("-------------------- Initalizing Web Server --------------------")
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADRESS
+    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
     await web.TCPSite(app, bind_address, Var.PORT).start()
-    print('----------------------------- DONE -----------------------------')
-    print('\n')
-    print('----------------------- Service Started -----------------------')
-    print('                        bot =>> {}'.format(bot_info.first_name))
+    print("----------------------------- DONE -----------------------------")
+    print()
+    print("----------------------- Service Started -----------------------")
+    print("                        bot =>> {}".format(bot_info.first_name))
     if bot_info.dc_id:
-        print('                        DC ID =>> {}'.format(str(bot_info.dc_id)))
-    print('                        server ip =>> {}'.format(bind_address, Var.PORT))
+        print("                        DC ID =>> {}".format(str(bot_info.dc_id)))
+    print("                        server ip =>> {}".format(bind_address, Var.PORT))
     if Var.ON_HEROKU:
-        print('                        app runnng on =>> {}'.format(Var.FQDN))
-    print('---------------------------------------------------------------')
+        print("                        app running on =>> {}".format(Var.FQDN))
+    print("---------------------------------------------------------------")
     await idle()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         loop.run_until_complete(start_services())
     except KeyboardInterrupt:
-        logging.info('----------------------- Service Stopped -----------------------')
+        logging.info("----------------------- Service Stopped -----------------------")
