7d70754fee297e7a98b9ee155fc088da69082fae
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-08 14:21:23 +0330

    Update __init__.py

--- Megatron/bot/__init__.py
+++ Megatron/bot/__init__.py
@@ -1,12 +1,19 @@
+from os import getcwd
+
 from pyrogram import Client
-from pyromod import listen
+
 from ..vars import Var
 
 StreamBot = Client(
-    session_name='Megatron',
+    session_name="Megatron",
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
+    workdir="Megatron",
+    plugins={"root": "Megatron/bot/plugins"},
     bot_token=Var.BOT_TOKEN,
     sleep_threshold=Var.SLEEP_THRESHOLD,
-    workers=Var.WORKERS
+    workers=Var.WORKERS,
 )
+
+multi_clients = {}
+work_loads = {}
