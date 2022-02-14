e30951b8e8fcaf5ce572822e77b8d7bfd17f7364
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-14 21:25:38 +0330

    Update antispam.py

--- Megatron/utils/antispam.py
+++ Megatron/utils/antispam.py
@@ -1,14 +1,21 @@
 import time
 
+from Megatron.vars import Var
+
 INTERVAL = {}
 
 async def check_spam(user_id: int):
     if str(user_id) in INTERVAL:
         now_time = time.time()
         last_time = INTERVAL[str(user_id)]
-        if round(now_time - last_time) < 120:
+        if round(now_time - last_time) < 120 and user_id not in Var.PRO_USERS:
             return True, round(last_time - now_time + 120)
-        elif round(now_time - last_time) >= 120:
+        elif round(now_time - last_time) >= 120 and user_id not in Var.PRO_USERS:
+            del INTERVAL[str(user_id)]
+            return False, None
+        elif round(now_time - last_time) < 10 and user_id in Var.PRO_USERS:
+            return True, round(last_time - now_time + 10)
+        elif round(now_time - last_time) >= 10 and user_id in Var.PRO_USERS:
             del INTERVAL[str(user_id)]
             return False, None
     elif str(user_id) not in INTERVAL:
