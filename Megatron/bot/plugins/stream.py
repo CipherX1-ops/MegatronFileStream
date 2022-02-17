e7ec20508f72c307b2188f162fbc85602ad4170e
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-17 20:25:56 +0330

    Update stream.py

--- Megatron/bot/plugins/stream.py
+++ Megatron/bot/plugins/stream.py
@@ -89,9 +89,15 @@ async def media_receive_handler(c: Client, m: Message):
         #d = [i for i in y]
         file = detect_type(m)
         file_name = ''
-        if file and "GiB" in str(file_size):
+        if file and "GiB" in str(file_size) and not m.from_user.id in Var.PRO_USERS:
             await c.send_message(m.chat.id, "⚜️ Files with size more than 1GiB need premium subscription. For purchasing premium subscription contact @CipherXBot.\n\n⚜️ امکان دریافت لینک فایل هایی با حجم بیشتر از 1 گیگ فقط برای کاربران پریمیوم امکان پذیر است. جهت خرید اشتراک پریمیوم و برداشته شدن محدودیت ها به @CipherXBot پیام دهید.")
         else:
+            is_spam, sleep_time = await check_spam(m.from_user.id)
+            if is_spam:
+                if m.from_user.id in Var.PRO_USERS:
+                    await m.reply_text(f"⚠️ Don't spam premium user\n✨ As you're a premium user you have to wait for `{str(sleep_time)}` seconds. Usual users have to wait for 120 seconds.\n\n⚠️ اسپم نزنید کاربر پریمیوم\n✨ با وجود کاربر پریمیوم بودن، شما باید `{str(sleep_time)}` ثانیه صبر کنید. کاربران عادی 120 ثانیه محدودیت دارند.", quote=True)
+                else:
+                    await m.reply_text(f"⚠️ Don't spam!\n✨ You have to wait for `{str(sleep_time)}` seconds or purchasing premium subscription via contacting @CipherXBot.\n\n⚠️ اسپم نزنید!\n✨ شما باید `{str(sleep_time)}` ثانیه صبر کنید و یا اشتراک پریمیوم از طریق ارتباط با @CipherXBot تهیه نمایید.", quote=True)
             file_name = file.file_name
             log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
             stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
@@ -111,13 +117,6 @@ async def media_receive_handler(c: Client, m: Message):
                 parse_mode="Markdown"
             )
             
-        is_spam, sleep_time = await check_spam(m.from_user.id)
-        if is_spam:
-            await m.reply_text(f"`⚠️ Don't spam!\n✨ You have to wait for `{str(sleep_time)}` seconds or purchasing premium subscription via contacting @CipherXBot.\n\n⚠️ اسپم نزنید!\n✨ شما باید `{str(sleep_time)}` ثانیه صبر کنید و یا اشتراک پریمیوم از طریق ارتباط با @CipherXBot تهیه نمایید.", quote=True)
-        if m.from_user.id in Var.PRO_USERS:
-            if is_spam:
-                await m.reply_text(f"`⚠️ Don't spam premium user\n✨ As you're a premium user you have to wait for `{str(sleep_time)}` seconds. Usual users have to wait for 120 seconds.\n\n⚠️ اسپم نزنید کاربر پریمیوم\n✨ با وجود کاربر پریمیوم بودن، شما باید `{str(sleep_time)}` ثانیه صبر کنید. کاربران عادی 120 ثانیه محدودیت دارند.", quote=True)
-
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
         await asyncio.sleep(e.x)
