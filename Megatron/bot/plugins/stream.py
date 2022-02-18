f6f0b0551cee434c4f16eae1f4264efba8a88386
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-19 01:05:30 +0330

    Update stream.py

--- Megatron/bot/plugins/stream.py
+++ Megatron/bot/plugins/stream.py
@@ -87,14 +87,14 @@ async def media_receive_handler(c: Client, m: Message):
         file = detect_type(m)
         file_name = ''
         if file and "GiB" in str(file_size) and not m.from_user.id in Var.PRO_USERS:
-            await c.send_message(m.chat.id, "⚜️ Files with size more than 1GiB need premium subscription. For purchasing premium subscription contact @CipherXBot.\n\n⚜️ امکان دریافت لینک فایل هایی با حجم بیشتر از 1 گیگ فقط برای کاربران پریمیوم امکان پذیر است. جهت خرید اشتراک پریمیوم و برداشته شدن محدودیت ها به @CipherXBot پیام دهید.")
+            await c.send_message(m.chat.id, "⚜️ Files with size more than 1GiB need premium subscription. For purchasing premium subscription press /upgrade command.\n\n⚜️ امکان دریافت لینک فایل هایی با حجم بیشتر از 1 گیگ فقط برای کاربران پریمیوم امکان پذیر است. جهت خرید اشتراک پریمیوم و برداشته شدن محدودیت ها کامند /upgrade را وارد نمایید.")
         else:
             is_spam, sleep_time = await check_spam(m.from_user.id)
             if is_spam:
                 if m.from_user.id in Var.PRO_USERS:
                     await m.reply_text(f"⚠️ Don't spam premium user\n\n✨ As you're a premium user you have to wait for `{str(sleep_time)}` seconds. Usual users have to wait for 120 seconds.\n\n⚠️ اسپم نزنید کاربر پریمیوم\n✨ با وجود کاربر پریمیوم بودن، شما باید `{str(sleep_time)}` ثانیه صبر کنید. کاربران عادی 120 ثانیه محدودیت دارند.", quote=True)
                 else:
-                    await m.reply_text(f"⚠️ Don't spam!\n\n✨ You have to wait for `{str(sleep_time)}` seconds or purchasing premium subscription via contacting @CipherXBot.\n\n⚠️ اسپم نزنید!\n✨ شما باید `{str(sleep_time)}` ثانیه صبر کنید و یا اشتراک پریمیوم از طریق ارتباط با @CipherXBot تهیه نمایید.", quote=True)
+                    await m.reply_text(f"⚠️ Don't spam!\n\n✨ You have to wait for `{str(sleep_time)}` seconds or purchasing premium subscription via /upgrade command.\n\n⚠️ اسپم نزنید!\n✨ شما باید `{str(sleep_time)}` ثانیه صبر کنید و یا اشتراک پریمیوم از طریق کامند /upgrade اشتراک پریمیوم تهیه نمایید.", quote=True)
             else:
                 file_name = file.file_name
                 user_type = "#Pro_User" if m.from_user.id in Var.PRO_USERS else "#Ordinary_User" 
