12c463bcdb2b49710b30d492879cea7115f54075
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-02-27 00:54:23 +0330

    Update start.py

--- Megatron/bot/plugins/start.py
+++ Megatron/bot/plugins/start.py
@@ -7,7 +7,8 @@ from Megatron.vars import Var
 from Megatron.utils.human_readable import humanbytes
 from Megatron.utils.database import Database
 from Megatron.handlers.fsub import force_subscribe
- 
+from Megatron.strings import get_string
+
 db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 
 
@@ -27,7 +28,7 @@ async def start(b, m : Message):
             if fsub == 400:
                 return
         await m.reply(
-            text=f"""Hey Dear {m.from_user.mention(style="md")} 🙋🏻‍♂️\nI'm Telegram File to Link Generator Bot.\n\nSend me any file & get the fast direct download link!\n\n⚠ **Don't forget to read the [rules](https://t.me/FutureTechnologyOfficial/1257) first!**\n\nسلام {m.from_user.mention(style="md")} عزیز 🙋🏻‍♂️\nمن بات تبدیل فایل به لینک هستم\nفایل تلگرامی خود را ارسال کنید تا لینک دانلود پر سرعت آن را دریافت نمایید\n\n**⚠ لطفا قبل از استفاده از بات [قوانین](https://t.me/FutureTechnologyOfficial/1257) را مطالعه نمایید!**\n\nهمچنین با زدن دستور /nim می‌توانید لینک های دریافتی خود را نیم بها نمایید.""",
+            text=get_string("start").format(m.from_user.mention(style="md")),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton('✵ Updates Channel ✵', url='https://t.me/FutureTechnologyOfficial'), InlineKeyboardButton('✵ Support Group ✵', url='https://t.me/joinchat/riq-psSksFtiMDU8')],
@@ -45,7 +46,7 @@ async def start(b, m : Message):
         if u.status == "kicked" or u.status == "banned":
             await b.send_message(
                 chat_id=m.from_user.id,
-                text="✨ You're Banned due not to pay attention to the [rules](https://t.me/FutureTechnologyOfficial/1257). Contact [Support Group](https://t.me/joinchat/riq-psSksFtiMDU8) if you think you've banned wrongly.\n\n✨ شما به علت عدم رعایت [قوانین](https://t.me/FutureTechnologyOfficial/1257) بن شده اید. اگر فکر میکنید بن شدن شما اشتباه بوده و قوانین را رعایت کرده اید می توانید با [گروه پشتیبانی](https://t.me/joinchat/riq-psSksFtiMDU8) در ارتباط باشید.",
+                text=get_string("banned"),
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -53,7 +54,7 @@ async def start(b, m : Message):
 @StreamBot.on_message(filters.command('upgrade') & filters.private & ~filters.edited)
 async def upgrade(b, m : Message):
     await m.reply(
-        text=f"""─────────────\n➰ **Daily Plan :**\n✗ 5 days - 5 dollar\n✗ 10 days - 10 dollar\n✗ 20 days - 20 dollar\n••••••••••••••••\n➰ **Monthly Plan :**\n✗ 1 month - 30 dollar\n✗ 3 months - 80 dollar\n✗ 6 months - 150 dollar\n••••••••••••••••\n➰ **Annual Plan :**\n✗ 1 year - 290 dollar\n─────────────\n─────────────\n➰ **پلن روزانه :**\n✗ 5 روزه - 5000 تومان \n✗ 10 روزه - 10000 تومان\n✗ 20 روزه - 20000 تومان\n••••••••••••••••\n➰ **پلن ماهانه :**\n✗ 1 ماهه - 30000 تومان\n✗ 3 ماهه - 80000 تومان\n✗ 6 ماهه - 150000 تومان\n••••••••••••••••\n➰ **پلن سالانه :**\n✗ 1 ساله - 290000 تومان\n─────────────\n✨ برای خرید روی دکمه زیر کلیک کنید""",
+        text=get_string("pay"),
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton('✨ Buy & Contact ✨', url='https://t.me/CipherXBot')]
@@ -78,13 +79,13 @@ async def help_handler(b, m : Message):
     if u.status == "kicked" or u.status == "banned":
         await b.send_message(
             chat_id=m.from_user.id,
-            text="✨ You're Banned due not to pay attention to the [rules](https://t.me/FutureTechnologyOfficial/1257). Contact [Support Group](https://t.me/joinchat/riq-psSksFtiMDU8) if you think you've banned wrongly.\n\n✨ شما به علت عدم رعایت [قوانین](https://t.me/FutureTechnologyOfficial/1257) بن شده اید. اگر فکر میکنید بن شدن شما اشتباه بوده و قوانین را رعایت کرده اید می توانید با [گروه پشتیبانی](https://t.me/joinchat/riq-psSksFtiMDU8) در ارتباط باشید.",
+            text=get_string("banned"),
             parse_mode="markdown",
             disable_web_page_preview=True
         )
     else:
         await m.reply_text(
-            text="✨ Send me any file, I'll give you its direct download link\n\nAlso I'm supported in channels. Add me to channel as admin to make me workable\n\n✨ فایل تلگرامی خود را برای من بفرستید تا لینک دانلود مستقیم آن را برای شما بفرستم\n\nهمچنین با ارتقای من به عنوان ادمین در چنل خود می توانید از امکانات من استفاده کنید، بدین صورت که در زمان پست فایل جدید در چنل دکمه شیشه ای دریافت لینک مستقیم فایل پست شده در زیر پست ایجاد می گردد.",
+            text=get_string("help"),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
