b439b3a72c3febaaaf573ccf23aa3484d57dd623
CoderX <66735676+ToxygenX@users.noreply.github.com>
2022-08-03 18:09:08 +0430

    Update __main__.py

--- Megatron/__main__.py
+++ Megatron/__main__.py
@@ -1,56 +1,80 @@
+import sys
 import asyncio
 import logging
 from .vars import Var
 from aiohttp import web
 from pyrogram import idle
 from Megatron import utils
-from Megatron import bot_info
+from Megatron import StreamBot
 from Megatron.server import web_server
 from Megatron.bot.clients import initialize_clients
 
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+    level=logging.INFO,
+    datefmt="%d/%m/%Y %H:%M:%S",
+    format="[%(asctime)s][%(levelname)s] => %(message)s",
+    handlers=[logging.StreamHandler(stream=sys.stdout),
+              logging.FileHandler("streambot.log", mode="a", encoding="utf-8")],)
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-loop = asyncio.get_event_loop()
+server = web.AppRunner(web_server())
 
+if sys.version_info[1] > 9:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+else:
+    loop = asyncio.get_event_loop()
+    
 
 async def start_services():
-    print("----------------------------- DONE -----------------------------")
+    print()
+    print("-------------------- Initializing Telegram Bot --------------------")
+    await StreamBot.start()
+    bot_info = await StreamBot.get_me()
+    StreamBot.username = bot_info.username
+    print("------------------------------ DONE ------------------------------")
     print()
     print(
-        "----------------------------- Initializing Clients -----------------------------"
+        "---------------------- Initializing Clients ----------------------"
     )
     await initialize_clients()
-    print("----------------------------- DONE -----------------------------")
+    print("------------------------------ DONE ------------------------------")
     if Var.ON_HEROKU:
         print("------------------ Starting Keep Alive Service ------------------")
         print()
         asyncio.create_task(utils.ping_server())
-    print("-------------------- Initalizing Web Server --------------------")
-    app = web.AppRunner(await web_server())
-    await app.setup()
+    print("--------------------- Initalizing Web Server ---------------------")
+    await server.setup()
     bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
-    await web.TCPSite(app, bind_address, Var.PORT).start()
-    print("----------------------------- DONE -----------------------------")
+    await web.TCPSite(server, bind_address, Var.PORT).start()
+    print("------------------------------ DONE ------------------------------")
     print()
-    print("----------------------- Service Started -----------------------")
+    print("------------------------- Service Started -------------------------")
     print("                        bot =>> {}".format(bot_info.first_name))
     if bot_info.dc_id:
         print("                        DC ID =>> {}".format(str(bot_info.dc_id)))
     print("                        server ip =>> {}".format(bind_address, Var.PORT))
     if Var.ON_HEROKU:
         print("                        app running on =>> {}".format(Var.FQDN))
-    print("---------------------------------------------------------------")
+    print("------------------------------------------------------------------")
     await idle()
 
+async def cleanup():
+    await server.cleanup()
+    await StreamBot.stop()
 
 if __name__ == "__main__":
     try:
         loop.run_until_complete(start_services())
     except KeyboardInterrupt:
-        logging.info("----------------------- Service Stopped -----------------------")
+        pass
+    except Exception as err:
+        logging.error(err.with_traceback(None))
+    finally:
+        loop.run_until_complete(cleanup())
+        loop.stop()
+        print("------------------------ Stopped Services ------------------------")
