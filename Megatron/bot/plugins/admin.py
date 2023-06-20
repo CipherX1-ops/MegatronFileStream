eacde743e73faa4dcce57aed8cf499669bc2cebb
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-06-20 19:31:39 +0330

    Update admin.py

--- Megatron/bot/plugins/admin.py
+++ Megatron/bot/plugins/admin.py
@@ -22,7 +22,7 @@ db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 broadcast_ids = {}
 
 
-@StreamBot.on_message(filters.command("status") & filters.private & filters.user(Var.OWNER_ID) & ~filters.edited)
+@StreamBot.on_message(filters.command("status") & filters.private & filters.user(Var.OWNER_ID))
 async def sts(c: Client, m: Message):
     total_users = await db.total_users_count()
     await m.reply_text(text=f"**Total Users in Database:** `{total_users}`", parse_mode="Markdown", quote=True)
